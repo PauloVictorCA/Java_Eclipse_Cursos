8c430c7a49492e3de77353d0c9a3d7cabd28f4a7
endp <paulovictor.ca@outlook.com>
2022-05-15 14:56:56 -0300

    Aula 16

--- Primeiros_Passos/src/Secao_IntroducaoSobreProgramacao/Aula_10.java
+++ Primeiros_Passos/src/Secao_IntroducaoSobreProgramacao/Aula_10.java
@@ -1,11 +1,21 @@
 package Secao_IntroducaoSobreProgramacao;
 
+import java.util.Locale;
+
 public class Aula_10 {
-
+	
 	public static void main(String[] args) {
-
+		Locale.setDefault(Locale.US);
+		
+		Double x = 2.578;
+		String y = "joão";
+		int z = 10;
+		
 		System.out.println("teste");
-
+		System.out.printf("%.2f\n", x);
+		System.out.printf("valor do x = %.2f metros \n", x);
+		System.out.println("teste");
+		System.out.printf("%s tem %d anos e sua conta possui %.2f reais", y, z, x);
 	}
 
 }
